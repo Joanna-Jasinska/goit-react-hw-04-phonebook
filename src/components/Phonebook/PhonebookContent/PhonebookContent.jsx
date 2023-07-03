65b03dde1802b514a255859fc6efcc1b75f5cf25
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-07-03 14:09:28 +0200

    refactor to class components

---
+++ src/components/Phonebook/PhonebookContent/PhonebookContent.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from 'react';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactList } from '../ContactList/ContactList';
+import { Title } from '../Title/Title';
+import { usePhonebook } from '../ContextProvider/ContextProvider';
+import css from './../Phonebook.module.css';
+export const PhonebookContent = () => {
+  const {
+    contacts,
+    setContacts,
+    name,
+    setName,
+    number,
+    setNumber,
+    filter,
+    setFilter,
+  } = usePhonebook();
+
+  const SaveContacts = () => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  };
+
+  useEffect(() => {
+    const loadedContacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    setContacts(loadedContacts);
+  }, []);
+  useEffect(() => {
+    SaveContacts();
+  }, [contacts]);
+
+  const addContactHandle = (e, newContact) => {
+    console.log(`Pcontent add Contact Handle`);
+    e.preventDefault();
+    const filtered =
+      contacts.find(
+        c => c.name.toLowerCase() === newContact.name.toLowerCase()
+      ) || 0;
+    if (filtered !== 0) {
+      setFilter(filtered.name);
+      alert(`You aready have ${filtered.name} in your phonebook.
+
+      ${filtered.name} ${filtered.number}`);
+      return;
+    }
+    const filteredNum =
+      contacts.find(
+        c => c.number.toLowerCase() === newContact.number.toLowerCase()
+      ) || 0;
+    if (filteredNum !== 0) {
+      alert(`You aready have this number in your phonebook.
+      
+      ${filteredNum.name} ${filteredNum.number}`);
+      setNumber(filteredNum.number);
+      return;
+    }
+    setContacts(prev => [...prev, { ...newContact }]);
+    setName('');
+    setNumber('');
+    setFilter('');
+  };
+  const deleteContactHandle = (e, contact) => {
+    e.preventDefault();
+    setContacts(prev => {
+      return [...prev.filter(c => c.id !== contact.id)];
+    });
+  };
+  return (
+    <div className={css.phonebook}>
+      <Title title="Phonebook" />
+
+      <ContactForm
+        name={name}
+        number={number}
+        addContactHandle={addContactHandle}
+        // inputOnChange={inputOnChange}
+        setName={setName}
+        setNumber={setNumber}
+      />
+
+      <Title title="Contacts" />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        // inputOnChange={inputOnChange}
+        setContacts={setContacts}
+        setFilter={setFilter}
+        deleteContactHandle={deleteContactHandle}
+      />
+    </div>
+  );
+};
