53a564dd9a8440352de13edd630846cd2a28b853
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-07-06 10:04:43 +0200

    removed dead code

--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './../Phonebook.module.css';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
